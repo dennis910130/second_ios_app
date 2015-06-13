2d815856e822084504d53bee3950ca7ba1513ab7
dennis910130 <sic046@ucsd.edu>
2015-06-12 22:30:39 -0700

    ios_app_2

--- play_grounds/ViewController.swift
+++ play_grounds/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var inputNumber: UITextField!
+    
+    @IBOutlet weak var showLabel: UILabel!
+    @IBAction func submitButton(sender: AnyObject){
+        var randNum = Int(arc4random_uniform(10)) + 1
+        var input = inputNumber.text.toInt()
+        if (input == randNum) {
+            showLabel.text = "Congrats, right!"
+        } else {
+            showLabel.text = "Wrong! \(randNum) is right"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
